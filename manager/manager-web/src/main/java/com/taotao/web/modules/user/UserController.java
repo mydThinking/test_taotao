76f5b38de5ef6a6ac26cd5f3f60bb88533f15895
thinking <814688419@qq.com>
2018-10-16 08:37:46 +0800

    展示层代码提交

--- manager/manager-web/src/main/java/com/taotao/web/modules/user/UserController.java
+++ manager/manager-web/src/main/java/com/taotao/web/modules/user/UserController.java
@@ -1,8 +1,11 @@
 package com.taotao.web.modules.user;
 
 import com.taotao.service.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * @author myd
@@ -11,13 +14,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/user")
 public class UserController {
 
-
-    UserService userService;
-
+    @Autowired
+    private UserService userService;
 
     @RequestMapping(value = "/index")
     public String list() {
-        userService.findOne();
+        List<String> list = userService.findOne();
+        list.forEach(item -> {
+            System.out.println(item);
+        });
         return "modules/user/userList";
     }
 }
